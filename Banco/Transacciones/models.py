983a14fbd689c9f814e4e5105425a64a0e0efdf2
KODA97 <acovamartinez@gmail.com>
2023-03-06 20:40:59 -0600

    URL
    creacion de nuevas urls

--- Banco/Transacciones/models.py
+++ Banco/Transacciones/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Cuenta(models.Model):
+    saldo = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def depositar(self, monto):
+        self.saldo += monto
+        self.save()
+
+    def retirar(self, monto):
+        if self.saldo >= monto:
+            self.saldo -= monto
+            self.save()
--- Banco/Transacciones/views.py
+++ Banco/Transacciones/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Cuenta
 
 # Create your views here.
+def depositar(request, cuenta_id):
+    cuenta = get_object_or_404(Cuenta, pk=cuenta_id)
+    if request.method == 'POST':
+        monto = float(request.POST['monto'])
+        cuenta.depositar(monto)
+    return render(request, 'depositar.html', {'cuenta': cuenta})
+
+def retirar(request, cuenta_id):
+    cuenta = get_object_or_404(Cuenta, pk=cuenta_id)
+    if request.method == 'POST':
+        monto = float(request.POST['monto'])
+        cuenta.retirar(monto)
+    return render(request, 'retirar.html', {'cuenta': cuenta})
